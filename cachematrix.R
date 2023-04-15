dc957e3c588544c0c0a370eea71a3b60c8fd4406
Sabitha Mathew <sabitha@sabitha-mac.local>
2023-04-15 23:35:40 +1200

    updated file

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,14 @@
 ## setinv variable have the inverse matrix
 ## getinv variable retrieves the  value of variable 'inv'
 makeCacheMatrix <- function(x = matrix()) {
-      inv <- NULL                       ## initializes the variable inv with NULL
-      set <- function(y) {              ##set the value of matrix
-        x <<- y                         ## set the value to a global environment
-        inv <<- NULL                    ## reset inv to NULL
+      inv <- NULL                                 ## initializes the variable inv with NULL
+      set <- function(y) {                        ##set the value of matrix
+        x <<- y                                   ## set the value to a global environment
+        inv <<- NULL                              ## reset inv to NULL
       }
       get <- function() x
-      setinv <- function(solve) inv <<- solve ##set the value of inverse matrix ie solve
-      getinv <- function() inv ## get the value of variable inv which have the inverse matrix
+      setinv <- function(solve) inv <<- solve     ##set the value of inverse matrix ie solve
+      getinv <- function() inv                    ## get the value of variable inv which have the inverse matrix
       list(set = set, get = get, setinv = setinv, getinv = getinv) 
 }
 
@@ -20,12 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
       ## Return a matrix that is the inverse of 'x' matrix
       inv <- x$getinv()
-      if(!is.null(inv)) {             ## checking if cached data present
+      if(!is.null(inv)) {                         ## checking if cached data present
         message("getting cached data")
-        return(inv)                   ## if cached data (inversed matrix) exist then return it
+        return(inv)                               ## if cached data (inversed matrix) exist then return it
       }
       data <- x$get()
-      inv <- solve(data, ...)         ## if cached data does not exist then calculate inverse matrix
+      inv <- solve(data, ...)                     ## if cached data does not exist then calculate inverse matrix
       x$setinv(inv)
       inv
   
